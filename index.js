2a423333a6598da32c8a79d4f72b5cff7c9d2d08
Jaffdavy <jaffdavy100@gmail.com>
2023-11-29 09:36:20 +0100

    functions

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function minNumber(a, b) {
+   if (a < b) {
+      return "a is smaller"
+   } else {
+      return "b is smaller"
+   }
+}
+console.log(Math.min(6, 9))
+
+
+function maxNumber(a, b) {
+     if (a < b) {
+        return "b is greater"
+     } else {
+        return "a is greater"
+     }
+}
+console.log(Math.max(10, 20))
+
+
+function sum(a, b) {
+    return (a + b)
+}
+console.log(sum(5, 5))
